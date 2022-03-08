6172ed5ffd3ac22c8294424a72fe72d3c8604921
gedadev <gjbg1234@gmail.com>
2022-03-07 19:47:41 -0600

    added the basic functions of the calculator

---
+++ main.js
@@ -0,0 +1,20 @@
+function add(x, y){
+    console.log(x + y);
+}
+
+function subtract(x, y){
+    console.log(x - y);
+}
+
+function multiply(x, y){
+    console.log(x * y);
+}
+
+function divide(x, y){
+    console.log(x / y);
+}
+
+add(2, 3);
+subtract(5, 2);
+multiply(3, 2);
+divide(5, 2);
